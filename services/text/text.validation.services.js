4568cf4de31cc3b4edf272ed94e256b94021f550
Mauro Jutzutz Sepet <mauro.jutzutz@gmail.com>
2022-01-08 15:23:27 -0600

    agregando calidacion palindromo

--- services/text/text.validation.services.js
+++ services/text/text.validation.services.js
@@ -2,9 +2,13 @@ exports.textValidationServices = async (text = "") => {
   try {
     if (!text.trim().length) return { valid: false };
     const newText = text.split("").reverse().join("");
+    const palindrome =
+      newText.replace(/ /g, "").toLocaleLowerCase() ===
+      text.replace(/ /g, "").toLocaleLowerCase();
 
     return {
       text: newText,
+      palindrome,
       valid: true,
     };
   } catch (error) {
